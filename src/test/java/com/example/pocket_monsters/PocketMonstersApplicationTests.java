c38e58698b8b72c563cee41be32b2a8bae9835b6
Kyla-Allen <kja.kyla.allen@gmail.com>
2019-03-13 14:24:50 -0500

    initial commit

---
+++ src/test/java/com/example/pocket_monsters/PocketMonstersApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.pocket_monsters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocketMonstersApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
